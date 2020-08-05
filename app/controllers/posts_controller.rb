670ed82d8e9288bde62043f6b5cda2db75c9d9e4
keisuke kamioka <keee2404@gmail.com>
2020-08-05 01:11:29 +0000

    [Update] post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,42 @@
 class PostsController < ApplicationController
 	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.new(post_params)
+		if post.save
+			 redirect_to post_path(@post), notice: "記録しました。"
+		else
+			 render "new"
+		end
 	end
 
   def index
+  	@posts = Post.all
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
+  	@post = Post.find(params[:id])
   end
 
   def update
+  	@post = Post.find(params[:id])
+  	if @post.update(post_params)
+       redirect_to post_path(@post), notice: "更新しました。"
+    else
+       render "edit"
+    end
   end
 
   def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+    redirect_to posts_path
   end
 
   private
